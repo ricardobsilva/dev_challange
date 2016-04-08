6448101488257c7542363f711a7db614ccb77e71
ricardosilva13 <ricardo.b.silva13@gmail.com>
2016-04-07 22:19:44 -0300

    model classroom implemented

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,2 +1,5 @@
 class Classroom < ActiveRecord::Base
+  validates :student_id, :course_id,  presence: :true
+  belongs_to :student
+  belongs_to :course
 end
--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Classroom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:student_id) }
+  it { is_expected.to validate_presence_of(:course_id) }
+  it { is_expected.to belong_to(:student)}
+  it { is_expected.to belong_to(:course)}
+
+  context 'is invalid' do
+    it 'has a valid factory' do
+      expect(build(:classroom)).to be_valid
+    end
+  end  
+
+  context 'is invalid' do
+    it 'without a student' do
+      classroom = build(:classroom, student_id: nil)
+      classroom.valid?
+      expect(classroom.errors[:student_id]).to include("can't be blank")
+    end
+    it 'without a course' do
+      classroom = build(:classroom, course_id: nil)
+      classroom.valid?
+      expect(classroom.errors[:course_id]).to include("can't be blank")
+    end
+  end
 end
